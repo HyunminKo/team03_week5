9166013c31cdf409f2a099272432912a610d8a54
Larva <ssh9112@naver.com>
2016-04-06 17:32:45 +0900

    edit AppTest gold input test

--- src/test/java/com/team03/week5/AppTest.java
+++ src/test/java/com/team03/week5/AppTest.java
@@ -3,25 +3,38 @@ import static org.junit.Assert.*;
 import org.junit.*;
 
 public class AppTest 
-
 {
-   @Test
-    public void testApp()
+	User user;
+	PlanType planType;
+	TotalRate totalRate;
+   
+	@Test
+    public void setUp()
     {
-        assertEquals("","" );
+        user = new User();
+        planType = null;
+        totalRate = new TotalRate();
     }
-   @Test
-   public void UserSmallGold()
-   {
-       User user = new User();
-       user.setPlanTypeName("gold");
+	@Test
+	public void UserSmallGold()
+	{
+		user.setPlanTypeName("gold");
        assertEquals("gold", user.getPlanTypeName());
-   }
+	}
    @Test
    public void UserBigGold()
    {
        User user = new User();
        user.setPlanTypeName("Gold");
+       assertEquals("Gold", user.getPlanTypeName());
+   }
+   @Test
+   public void UserGoldBill()
+   {
+       User user = new User();
+       user.setPlanTypeName("Gold");
+       user.setNumberOfLines(1);
+       user.setMinutesUsed(500);
        assertEquals("Gold", user.getPlanTypeName());
    }
 }
